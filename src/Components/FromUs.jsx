44f8fcb1284ddf2b11f4367f0caad4a478354069
AgudaD <agudaoluwanifemi@gmail.com>
2023-04-07 20:47:19 +0100

    new commit

--- src/Components/FromUs.jsx
+++ src/Components/FromUs.jsx
@@ -3,32 +3,47 @@ import React from "react";
 const FromUs = () => {
   return (
     <div className="bg-white font-helvetica p-7 mt-10 md:px-32">
-      <p className="mt-24 text-[#6D4E56] text-xl font-adobe font-semibold italic md:text-center">From us</p>
+      <p className="mt-24 text-[#6D4E56] text-xl font-adobe font-semibold italic md:text-center">
+        PRESS RELEASE
+      </p>
       <p className="mt-8 mb-20 text-[#424F47] text-sm tracking-wide leading-6">
-        Lorem ipsum dolor sit amet consectetur. Ultrices ullamcorper amet urna
-        lobortis. Nibh eget amet quis mi. Cursus felis hendrerit tortor dictum
-        odio metus. Viverra euismod urna urna nisl ultrices id. Elit viverra sem
-        vel semper quam. Viverra lectus eu posuere porta id odio sit sed. Turpis
-        fermentum elit sed malesuada. Risus aliquam vitae viverra egestas ut
-        interdum nulla consectetur venenatis. Commodo et sed augue vitae sit at
-        volutpat fringilla urna. Id molestie est nam lectus scelerisque
-        fringilla egestas eu. Eu quam adipiscing diam nibh viverra sed turpis.
-        Urna risus ultrices nunc ipsum pulvinar. Gravida sit eget et
-        pellentesque nibh feugiat. Tortor turpis lacus arcu pulvinar volutpat
-        sapien sit scelerisque. Mauris etiam et sit ultricies felis nulla
-        eleifend turpis. Etiam commodo sollicitudin elementum et diam. Urna
-        sapien eu dignissim sit justo lorem mauris. Lobortis ullamcorper libero
-        at sollicitudin at faucibus rhoncus et. Etiam rhoncus tortor amet orci
-        amet scelerisque hac eleifend. Dui dui aliquam porta eget turpis
-        aliquam. Et amet ac integer tortor lectus sed non at consequat. Sagittis
-        cursus senectus sapien sapien neque viverra pellentesque nibh. Amet
-        lectus nunc quam mauris ac amet blandit malesuada dapibus. Enim sem
-        tortor id aliquam venenatis natoque. Eleifend curabitur condimentum
-        pulvinar nec nisl. Dolor purus sollicitudin vel feugiat lacinia urna
-        faucibus. Eget sit scelerisque elementum ultricies non augue non.
-        Molestie nulla amet quam aliquam. Rhoncus lectus sit a ut proin
-        maecenas. Arcu metus fringilla sit egestas. In ut amet at morbi. Gravida
-        id lorem amet eget enim in.
+        The Art Roost Gallery Debuts Art Exhibition “Run Through Walls” From the
+        8th to the 9th of April 2023, the Art Roost Gallery will present an
+        exhibition of paintings and other forms of art work. This exhibition's
+        theme reads “Run Through Walls” which is coined from the English
+        idiomatic expression “Run through a Brick wall”. This translates as the
+        ability to do whatever it takes to succeed, despite whatever hurdles are
+        in one's path. Established in 2021, Art Roost Gallery was founded by
+        four friends with a similar interest in Art, Fashion, Creative Design,
+        Architecture, and Real Estate. Tochukwu Ugah, Dara Shonubi, Chino
+        Okafor, and Mahmud Alabi have come together to shine a light on Art in
+        this day and age. As the name suggests “Art Roost” is a form of nest to
+        nurture and celebrate artists. It's also an avenue to showcase young
+        artists who are striving to be in the art space and also to recognize
+        and pay homage to past artists whose artworks make a difference in the
+        world. With their first exhibition in the plan, it would also sensitize
+        the public on the importance of art and also foster an art community in
+        Nigeria. The exhibition would feature known and upcoming artists
+        presenting their artworks which gives a glimpse into their artistic
+        style, personal stories, and preferences. It would feature artists such
+        as Pascal Ugwu and Tobiloba Fasalejo. Born in 1996 in Enugu state,
+        Paschal Ugwu resides in Abuja where he creates jaw-dropping artworks.
+        Ugwu's art style leans closer to the creation of humans. Ugwu's work is
+        a powerful expression of the human form and a thought-provoking
+        commentary on the human experience. He has also been featured in
+        publications such as "Shoutout Miami", Los Angeles, 2022, and
+        "Artmatazz" Art magazine. Tobiloba Fasalejo studies Fine and Applied
+        Arts at Obafemi Awolowo University to explore his artistic sense. Right
+        from childhood, Tobi has had a liking for art and presently uses his
+        artwork as a form to expose and shed light on black identity, in all
+        spheres. Both artists have participated in other exhibitions such as a
+        group exhibition "In Time like This" by Orange Gallery in Calabar,
+        Nigeria.“The Manifesto I & II", at the Nnamdi Azikiwe Library,
+        University of Nigeria, Nsukka, 2020. "Dreams Alive 13", at the Marlima
+        Basement in the University of Nigeria, Nsukka (2018). Their partnership
+        with the Art Roost Gallery is to showcase more of their incredible
+        artworks and connect with other art enthusiasts in the Lagos art scene
+        and it's diaspora.
       </p>
     </div>
   );
