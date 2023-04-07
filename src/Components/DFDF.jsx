d196a5512233407b271db0086e6cb3835f19bbee
AgudaD <agudaoluwanifemi@gmail.com>
2023-04-07 20:42:32 +0100

    new commit

--- src/Components/DFDF.jsx
+++ src/Components/DFDF.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Autoplay, EffectFade } from "swiper";
@@ -8,12 +7,9 @@ import chino47 from "../assets/chino47.jpg";
 import chino55 from "../assets/chino55.jpg";
 import chino61 from "../assets/chino61.jpg";
 import art1 from "../assets/art1.png";
-// import art3 from "../assets/art3.png";
 import art4 from "../assets/art4.png";
 
 const DFDF = () => {
-  const [isActive, setIsActive] = useState(false);
-
   return (
     <div className="bg-[#849E8F] p-2">
       <Swiper
@@ -43,9 +39,7 @@ const DFDF = () => {
         className="w-full px-4 py-6 mt-20  cursor-pointer "
       >
         <div className="flex justify-center items-center gap-12">
-          <SwiperSlide style={{
-            transform: isActive ? 'rotate(-11deg)' : 'none'
-          }} on={() => setIsActive(!isActive)}>
+          <SwiperSlide>
             <img src={art1} alt="" className="w-[300px] h-80" />
           </SwiperSlide>
           <SwiperSlide>
